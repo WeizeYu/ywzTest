17451a5193b0bfc32cf921028ebfdb2f2f8931cc
yuweize <604974255@qq.com>
2016-03-14 16:08:19 +0800

    firsttest by yu

---
+++ yu-test/yu-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yu-test
+//
+//  Created by yuweize on 16/3/14.
+//  Copyright © 2016年 yuweize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yu-test/yu-test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yu-test
+//
+//  Created by yuweize on 16/3/14.
+//  Copyright © 2016年 yuweize. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
